54e67014decf36ae57a38e42c8c5e30d61ce0e8e
Celie Baker <celie.n133@gmail.com>
2018-11-13 14:01:30 -0700

    deployed to server

--- vuejs-fundamentals/src/cart/ShoppingCart.vue
+++ vuejs-fundamentals/src/cart/ShoppingCart.vue
@@ -18,7 +18,7 @@
             {{robot.head.title}}
           </td>
           <td class="cost">
-            {{robot.cost | currency('£')}}
+            {{robot.cost | currency('$')}}
           </td>
         </tr>
       </tbody>
@@ -41,7 +41,7 @@
             {{robot.head.title}}
           </td>
           <td class="cost">
-            {{robot.cost | currency('£')}}
+            {{robot.cost | currency('$')}}
           </td>
         </tr>
       </tbody>
@@ -50,7 +50,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'Cart',
   computed: {
